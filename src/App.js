560d688e40b46691191719d772d70205d3696dc6
Mamed Guseinov <mameguseynov@mail.ru>
2024-11-12 22:21:59 +0300

    Убраны ненужные элементы из главного файла

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Common/Navbar';
+import ExpertDashboard from './components/Expert/ExpertDashboard';
+import UserDashboard from './components/User/UserDashboard';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Navigate to="/expert" />} />
+
+        <Route path="/expert/*" element={<ExpertDashboard />} />
+
+        <Route path="/user" element={<UserDashboard />} />
+
+      </Routes>
+
+    </Router>
   );
 }
 
